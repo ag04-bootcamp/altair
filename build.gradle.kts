e93ef423700ccb34d517d6cd421b430f67ffa93d
Ana Malinar <ana@Anas-MacBook-Pro.local>
2022-05-10 09:42:36 +0200

    add basic dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,16 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-parent
+    implementation("org.springframework.boot:spring-boot-starter-parent:2.2.5.RELEASE")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-webflux
+    implementation("org.springframework.boot:spring-boot-starter-webflux:2.4.5")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.module/jackson-module-kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    // https://mvnrepository.com/artifact/org.springframework.data/spring-data-r2dbc
+    implementation("org.springframework.data:spring-data-r2dbc:1.4.0")
+
+
 }
 
 tasks.test {
