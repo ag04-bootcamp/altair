914e50a9c4fd37033b5c6e3fff0b18d09ce9e3a9
Ana Malinar <ana@Anas-MacBook-Pro.local>
2022-05-12 11:11:57 +0200

    fix avg calculation

--- src/main/kotlin/proba/controllers/HealthRecordController.kt
+++ src/main/kotlin/proba/controllers/HealthRecordController.kt
@@ -13,6 +13,8 @@ import reactor.core.publisher.Mono
 @RestController
 class HealthRecordController(val repository: HealthRecordRepository) {
 
+    private var cnt: Int = 0
+
     @PostMapping("/health/{profileId}/record")
     fun storeHealthRecord(@PathVariable("profileId") profileId: Long,
                           @RequestBody record: HealthRecord): Mono<HealthRecord> =
@@ -20,17 +22,22 @@ class HealthRecordController(val repository: HealthRecordRepository) {
 
     @GetMapping("/health/{profileId}/avg")
     fun fetchHealthRecordAverage(@PathVariable("profileId") profileId: Long): Mono<AverageHealthStatus> =
-                            repository.findByProfileId(profileId)
-                                .reduce (fun(acc, record):HealthRecord {
-                                    acc.bloodPressure + record.bloodPressure
-                                    acc.heartRate + record.heartRate
-                                    acc.temperature + record.temperature
-                                    return acc
-                                })
-                                .map { s ->
-                                    val cnt = repository.findByProfileId(profileId).prefetch
-                                    return@map AverageHealthStatus(cnt, s.temperature/cnt,
-                                        s.bloodPressure/cnt,
-                                        s.heartRate/cnt)
-                                }
+            repository.findByProfileId(profileId)
+                .doFirst {
+                    cnt = 1
+                }
+                .reduce(fun(acc, record): HealthRecord {
+                    ++cnt
+                    acc.bloodPressure += record.bloodPressure
+                    acc.heartRate += record.heartRate
+                    acc.temperature += record.temperature
+                    return acc
+                })
+                .map { s ->
+                    return@map AverageHealthStatus(
+                        cnt, s.temperature / cnt,
+                        s.bloodPressure / cnt,
+                        s.heartRate / cnt
+                    )
+                }
 }
