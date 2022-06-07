3e344022214aa46470031672c02d7c9c190cc63b
ante <ante.latincic@ag04.com>
2022-06-07 12:20:47 +0200

    update-profile component

--- frontend/altair/src/components/update-profile/update-profile.component.tsx
+++ frontend/altair/src/components/update-profile/update-profile.component.tsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import DatePicker from "react-date-picker";
-import { stringify } from "querystring";
 
 let i = 0;
 
